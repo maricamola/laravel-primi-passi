52b0901ddd9e1bce23af79509f35c55004a92cd8
maricamola <maricamola98@libero.it>
2023-06-01 14:39:46 +0200

    blade added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $text = [
+        'welcome' => 'This is my frist home page',
+    ];
+    return view('home', $text);
 });
